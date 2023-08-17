b5b6b1e5e2afe1b2378b9117dc8efbdf4c13e4a4
Bonnie <onedrinkthreebars@gmail.com>
2023-08-16 21:40:45 -0400

    placeholder

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import Head from "next/head";
-import ContentWrapper from "../components/ContentWrapper";
-
 export default function Home() {
   return (
-    <ContentWrapper>
+    <div>
       coming soon
-    </ContentWrapper>
+    </div>
   );
 }
