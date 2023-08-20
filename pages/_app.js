4ee8b962ca23634149f239c23690a049cf6aff61
Bonnie <onedrinkthreebars@gmail.com>
2023-08-20 14:43:11 -0400

    moved global styles to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,17 @@
-import { GlobalStyles } from './App.styled';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+html,
+body {
+    padding: 0;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+h1 {
+  margin-block-start: 0;
+}
+`;
+
 
 function MyApp({ Component, pageProps }) {
   return (
