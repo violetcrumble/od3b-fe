6af9854fabb5306a1ab5baf72b5382ad59c8d1e6
Bonnie <bonniemellott@gmail.com>
2024-03-26 09:24:54 -0400

    comment out amazon pixel

--- components/Cards/AmazonListingCard/AmazonListingCard.js
+++ components/Cards/AmazonListingCard/AmazonListingCard.js
@@ -20,7 +20,7 @@ export default function AmazonListingCard(props) {
           border="0" 
           src={props.amazonPhotoURL} />}
         
-        <img src={`https://ir-na.amazon-adsystem.com/e/ir?t=onedrinkthree-20&language=en_US&l=li2&o=1&a=${props.amazonASIN}`} width="1" height="1" border="0" alt="" />
+        {/* <img src={`https://ir-na.amazon-adsystem.com/e/ir?t=onedrinkthree-20&language=en_US&l=li2&o=1&a=${props.amazonASIN}`} width="1" height="1" border="0" alt="" /> */}
       </div>
     </AmazonListingCardStyles>
   );
