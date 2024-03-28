7606f0805de2e85ddf255e3f93283bf463028576
Bonnie <bonniemellott@gmail.com>
2024-03-28 10:09:19 -0400

    add cachaca category and update content

--- components/CategoryNavPills/CategoryNavPills.js
+++ components/CategoryNavPills/CategoryNavPills.js
@@ -6,7 +6,9 @@ export default function CategoryNavPills(props) {
         <li onClick={() => props.setFilteredRecipes(props.filterRecipesByCategory("mezcal", props.recipes))}>Mezcal Cocktails</li>
         <li onClick={() => props.setFilteredRecipes(props.filterRecipesByCategory("whiskey", props.recipes))}>Whiskey Cocktails</li>
         <li onClick={() => props.setFilteredRecipes(props.filterRecipesByCategory("rum", props.recipes))}>Rum Cocktails</li>
+        <li onClick={() => props.setFilteredRecipes(props.filterRecipesByCategory("cachaça", props.recipes))}>Cachaça Cocktails</li>
         <li onClick={() => props.setFilteredRecipes(props.filterRecipesByCategory("gin", props.recipes))}>Gin Cocktails</li>
         <li onClick={() => props.setFilteredRecipes(props.filterRecipesByCategory("vodka", props.recipes))}>Vodka Cocktails</li>
+        
     </CategoryNavPillsStyles>;
 }
