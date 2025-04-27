4369f6f09eeb7786488f62c1259c8c406110c3f0
Bonnie <bonniemellott@gmail.com>
2025-04-27 11:50:01 -0400

    continuing the refactor, adding custom colors

--- components/HeroImage/HeroImage.js
+++ components/HeroImage/HeroImage.js
@@ -5,7 +5,7 @@ import heroBanner from '../../public/hero5.jpg';
 
 export default function HeroImage() {
   return (
-    <div className="relative">
+    <div className="relative w-100% h-150">
       <div className="z--1">
         <Image
           priority
@@ -18,14 +18,13 @@ export default function HeroImage() {
       </div>
 
       <div className="relative m-0 20px">
-      <h3>Empowering you to create craft cocktails at home</h3>
-      <Link href="https://www.youtube.com/channel/UCicZ2KV8_1cIKPI_82KI_AQ" target="_blank" className="youtube-button">Watch Our YouTube Videos</Link>
-          
+        
+        <h3 className="text-white text-3xl text-shadow-lg/20 p-50 0 0 0">
+          Empowering you to create craft cocktails at home</h3>
+        <Link href="https://www.youtube.com/channel/UCicZ2KV8_1cIKPI_82KI_AQ" target="_blank" 
+          className="text-white bg-brand-tertiary no-underline p-5 font-bold text-shadow-lg/20 rounded-md">Watch Our YouTube Videos</Link>
+
       </div>
     </div>
   );
 }
-
-
-
-
--- components/HeroImage/HeroImage.styled.js
+++ components/HeroImage/HeroImage.styled.js
@@ -2,41 +2,20 @@ import styled from 'styled-components';
 import { themeColors, breakpoints } from '../../utils/stylevars';
 
 export const HeroStyles = styled.div`
-    position: relative;
-    width: 100%;
+    
 
-    .imageWrapper {
-      z-index: -1;
-    }
   
   .heroContent {
-    position: relative;
-    margin: 0 20px;
-
-    h3 {
-      color: #fff;
-      font-size: 32px;
-      text-shadow: black 0.1em 0.1em 0.2em;
-      padding: 50px 0 0 0;
-    }
-
-    a {
-      color: #fff;
-    }
+    
 
     .youtube-button {
       background-color: ${themeColors.brandColorTertiary};
-      color: white;
       text-decoration: none;
-      padding: 10px;
-      font-weight: bold;
-      text-shadow: black 0.1em 0.1em 0.2em;
-      border-radius: 5px;
     }
   }
 
   @media (min-width: ${breakpoints.md}) {
-    height: 60vh;
+
 
     .heroContent {
       max-width: 780px;
