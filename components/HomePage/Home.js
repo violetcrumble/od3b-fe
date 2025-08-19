3ee51d9dc4fea37ea285e3e4fecbfc71b8b74fd2
Bonnie <bonniemellott@gmail.com>
2025-08-19 06:52:44 -0400

    small cleanup of css

--- components/HomePage/Home.js
+++ components/HomePage/Home.js
@@ -1,4 +1,3 @@
-
 import Link from 'next/link';
 import HeroImage from '../HeroImage/HeroImage';
 import RecipeListingCard from '../Cards/RecipeListingCard/RecipeListingCard.js';
@@ -6,46 +5,75 @@ import filterRecipesByCategory from '../../utils/filterRecipesByCategory.js';
 import styles from './Home.module.scss';
 
 export default function HomePage({ recipes }) {
-
   return (
     <div className={styles['home-page']}>
-
       <HeroImage />
 
       <div className="constrained-content">
         <h1 className="text-brand-teal">Tequila Recipes</h1>
-        <p>Looking for something beyond a tequila sunrise? Whether you want the combination of tequila, lime juice, and orange liqueur in a margarita or you&apos;re in the mood for a crisp, refreshing ranch water, we&apos;re sure to have a tequila cocktail for you!</p>
+        <p>
+          Looking for something beyond a tequila sunrise? Whether you want the combination of tequila, lime juice, and
+          orange liqueur in a margarita or you&apos;re in the mood for a crisp, refreshing ranch water, we&apos;re sure
+          to have a <a href="/cocktail-recipes?category=tequila">tequila cocktail</a> for you!
+        </p>
         <div className="listings-3-col">
-        {filterRecipesByCategory("tequila", recipes).slice(0, 3).map((recipe, index) => (
-          <Link className="listing-card" key={index} href={`/cocktail-recipes/${recipe.attributes.recipeUrlSlug}`} rel="canonical">
-            <RecipeListingCard recipe={recipe} />
-          </Link>
-        ))}
+          {filterRecipesByCategory('tequila', recipes)
+            .slice(0, 3)
+            .map((recipe, index) => (
+              <Link
+                className="listing-card"
+                key={index}
+                href={`/cocktail-recipes/${recipe.attributes.recipeUrlSlug}`}
+                rel="canonical"
+              >
+                <RecipeListingCard recipe={recipe} />
+              </Link>
+            ))}
         </div>
       </div>
 
       <div className="constrained-content">
         <h1 className="text-brand-teal">Whiskey Recipes</h1>
-        <p>From classic bourbon old fashioneds to equal parts bangers like the paper plane, there&apos;s a whiskey cocktai recipe here for you.</p>
+        <p>
+          From classic bourbon old fashioneds to equal parts bangers like the paper plane, there&apos;s a{' '}
+          <a href="/cocktail-recipes?category=whiskey">whiskey cocktail recipe</a> here for you.
+        </p>
         <div className="listings-3-col">
-        {filterRecipesByCategory("whiskey", recipes).slice(0, 3).map((recipe, index) => (
-          <Link className="listing-card" key={index} href={`/cocktail-recipes/${recipe.attributes.recipeUrlSlug}`} rel="canonical">
-            <RecipeListingCard recipe={recipe} />
-          </Link>
-        ))}
+          {filterRecipesByCategory('whiskey', recipes)
+            .slice(0, 3)
+            .map((recipe, index) => (
+              <Link
+                className="listing-card"
+                key={index}
+                href={`/cocktail-recipes/${recipe.attributes.recipeUrlSlug}`}
+                rel="canonical"
+              >
+                <RecipeListingCard recipe={recipe} />
+              </Link>
+            ))}
         </div>
       </div>
 
       <div className="constrained-content">
         <h1 className="text-brand-teal">Rum Cocktail Recipes</h1>
-        <p>From tiki classics to espresso martini riffs, we have some great rum recipes!</p>
+        <p>
+          From tiki classics to espresso martini riffs, we have some great{' '}
+          <a href="/cocktail-recipes?category=rum">rum recipes</a>!
+        </p>
         <div className="listings-3-col">
-        {filterRecipesByCategory("rum", recipes).slice(0, 3).map((recipe, index) => (
-          <Link className="listing-card" key={index} href={`/cocktail-recipes/${recipe.attributes.recipeUrlSlug}`} rel="canonical">
-            <RecipeListingCard recipe={recipe} />
-          </Link>
-        ))}
-</div>
+          {filterRecipesByCategory('rum', recipes)
+            .slice(0, 3)
+            .map((recipe, index) => (
+              <Link
+                className="listing-card"
+                key={index}
+                href={`/cocktail-recipes/${recipe.attributes.recipeUrlSlug}`}
+                rel="canonical"
+              >
+                <RecipeListingCard recipe={recipe} />
+              </Link>
+            ))}
+        </div>
       </div>
     </div>
   );
