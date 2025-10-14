96ac72e64d23113859493cd9782da687dda2c969
Bonnie <bonniemellott@gmail.com>
2025-10-14 11:33:37 -0400

    content update

--- components/HomePage/Home.js
+++ components/HomePage/Home.js
@@ -31,7 +31,6 @@ export default function HomePage({ recipes }) {
             ))}
         </div>
       </div>
-
       <div className="constrained-content">
         <h1 className="text-brand-teal">Whiskey Recipes</h1>
         <p>
