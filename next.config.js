615013e95af57f77b90b77be06f25a956f9bbdc9
Bonnie <bonniemellott@gmail.com>
2026-07-16 14:04:35 -0400

    adding redirect fallback code

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
   images: {
     remotePatterns: [
@@ -26,6 +26,32 @@ const nextConfig = {
         pathname: '/onedrinkthreebars/image/**',
       },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'onedrinkthreebars.com',
+          },
+        ],
+        destination: 'https://www.cocktailunderground.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.onedrinkthreebars.com',
+          },
+        ],
+        destination: 'https://www.cocktailunderground.com/:path*',
+        permanent: true,
+      },
+    ];
   },
 };
 
