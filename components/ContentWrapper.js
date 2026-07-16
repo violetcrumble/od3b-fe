647948ce856826e712ffb9b9ce4c9573303b17d6
Bonnie <bonniemellott@gmail.com>
2026-07-16 14:20:09 -0400

    favicon set

--- components/ContentWrapper.js
+++ components/ContentWrapper.js
@@ -1,9 +1,17 @@
+import Head from 'next/head';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 
 export default function ContentWrapper({ children }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
       <Header />
 
       <main>{children}</main>
