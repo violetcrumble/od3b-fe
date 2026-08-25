fc0fceb56cdfd6830a6256b886fc50f3fb148718
Bonnie <bonniemellott@gmail.com>
2026-08-25 10:18:09 -0400

    adding variety to similar recipes

--- utils/getRelatedRecipes.js
+++ utils/getRelatedRecipes.js
@@ -1,8 +1,21 @@
+function hashSlug(slug) {
+  let hash = 0x811c9dc5;
+  for (let i = 0; i < slug.length; i += 1) {
+    hash ^= slug.charCodeAt(i);
+    hash = Math.imul(hash, 0x01000193);
+  }
+  return hash >>> 0;
+}
+
 export default function getRelatedRecipes(currentRecipe, allRecipes, limit = 3) {
   const currentSpirits = currentRecipe.spirits.map((spirit) => spirit.spirit);
 
-  return allRecipes
+  const matches = allRecipes
     .filter((recipe) => recipe.recipeUrlSlug !== currentRecipe.recipeUrlSlug)
-    .filter((recipe) => recipe.spirits.some((spirit) => currentSpirits.includes(spirit.spirit)))
-    .slice(0, limit);
+    .filter((recipe) => recipe.spirits.some((spirit) => currentSpirits.includes(spirit.spirit)));
+
+  if (matches.length <= limit) return matches;
+
+  const offset = hashSlug(currentRecipe.recipeUrlSlug) % matches.length;
+  return Array.from({ length: limit }, (_, i) => matches[(offset + i) % matches.length]);
 }
