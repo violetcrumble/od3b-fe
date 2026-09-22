cc1215a9e470a1f1b2048c9731e810218906198e
Bonnie <bonniemellott@gmail.com>
2026-09-22 11:12:53 -0400

    reverting GA4 test change

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { montserrat } from '../utils/fonts';
-import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 import useAffiliateClickTracking from '../utils/trackAffiliateClicks';
 import useScrollDepthTracking from '../utils/trackScrollDepth';
 import '../styles/globals.scss';
 import '../styles/print.scss';
 
-// GA4 is loaded by the GTM container; a direct gtag script on top of it only duplicated 188 KB per page.
+// The GTM container only forwards dataLayer events; page views come from this direct GA4 tag. Do not remove it.
 const gtmId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER;
 const gtmEnabled = gtmId?.startsWith('GTM-');
 
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <div className={montserrat.className}>
         <Component {...pageProps} />
+        <GoogleAnalytics gaId="G-DG1D4YR0Y6" />
         {gtmEnabled && <GoogleTagManager gtmId={gtmId} />}
       </div>
     </>
